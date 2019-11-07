ee7b9796a1f32d1de2bfe049033a9b03a5a04bf7
Боря <Batishta_83@mail.ru>
2019-11-07 14:16:23 +0300

    Revert "HelloWorld start"
    This reverts commit d9065da9256fe6f4b0826c9cfef49c10d0fe83b4.
    
    # Conflicts:
    #	.idea/modules.xml
    #	HelloWorld.iml

---
+++ src/progwards/HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+package progwards.HelloWorld;
+
+public class HelloWorld {
+
+        static void println(String str){
+            System.out.println(str);
+        }
+        static int addituon(int x, int y) {
+            return x + y;
+        }
+
+
+        public static void  main(String[] args){
+            println("Привет, мир!");
+            println("Мир, ты слышишь меня?!");
+            System.out.print("Я знаю что 5 + 7 = ");
+            System.out.println(addituon(5,7));
+        }
+    }
+
+    //Комментарий
+
