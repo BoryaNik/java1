3b8fbd856437a1b86299f4534f052764e731c7a2
Боря <Batishta_83@mail.ru>
2019-11-11 10:54:48 +0300

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    static void println(String str){
+        System.out.println(str);
+    }
+    static int addituon(int x, int y) {
+        return x + y;
+    }
+
+
+    public static void  main(String[] args){
+        println("Test!");
+        println("Test!");
+        //System.out.print("");
+        System.out.println(addituon(5,7));
+    }
+}
