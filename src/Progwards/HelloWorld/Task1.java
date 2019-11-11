73645bd420b2c75862d79209ce52295225ff794d
Боря <Batishta_83@mail.ru>
2019-11-11 11:06:14 +0300

    Task1

---
+++ src/Progwards/HelloWorld/Task1.java
@@ -0,0 +1,10 @@
+package Progwards.HelloWorld;
+
+public class Task1 {
+    public static void main(String[] args){
+        String s= "Хорошо идут дела ";
+        String a= "Изучаю Java я!";
+        System.out.println(s+a);
+        System.out.println(a+s);
+    }
+}
