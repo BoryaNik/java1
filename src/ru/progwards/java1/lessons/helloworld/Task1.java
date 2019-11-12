26fdcf05fa337d269717e3715ea37c6d63e4c331
Боря <Batishta_83@mail.ru>
2019-11-12 14:05:22 +0300

    Task1

--- src/ru/progwards/java1/lessons/helloworld/Task1.java
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -4,6 +4,8 @@ public class Task1 {
     public static void main(String[] args){
         String s= "Хорошо идут дела ";
         String a= "Изучаю Java я!";
+        System.out.println("Хорошо идут дела");
+        System.out.println("Изучаю Java я!");
         System.out.println(s+a);
         System.out.println(a+s);
     }
