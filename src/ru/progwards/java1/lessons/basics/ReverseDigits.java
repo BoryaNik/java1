e3bae425843f83afb87f61bd69c61bc92b973d4a
Боря <Batishta_83@mail.ru>
2019-11-15 10:52:34 +0300

    ReverseDigits

---
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -0,0 +1,18 @@
+package ru.progwards.java1.lessons.basics;
+
+public class ReverseDigits {
+    public static void main(String[] args) {
+        reverseDigits(123);
+    }
+
+    public static void reverseDigits(long number) {
+        if (number < 10) {
+            System.out.println(number);
+            return;
+        }
+        else {
+            System.out.print(number % 10);
+            reverseDigits(number/10);
+        }
+    }
+}
