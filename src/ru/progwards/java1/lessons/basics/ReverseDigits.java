3aa0cc02c813d686e94c6ef3b341d113fbfaf29f
Боря <Batishta_83@mail.ru>
2019-11-19 10:23:52 +0300

    ReverseDigits

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -1,17 +1,18 @@
 package ru.progwards.java1.lessons.basics;
 
-public class ReverseDigits {
-    public static int reverseDigits(int number){
-    int a= number % 10;
-    int b= (number-a) / 10;
-    int c= b% 10;
-    int d= b/10;
+public class ReverseDigits{
+public static int reverseDigits(int number) {
+    int result=0;
+        while(number>0) {
+        result *= 10;
+        result += number % 10;
+        number /= 10;
+    }
+        System.out.println(result);
+            return result;
 
-        System.out.print(a);
-        System.out.print(c);
-        System.out.print(d);
-        return a;
 }
+
     public static void main (String[]args) {
         reverseDigits(123);
 
