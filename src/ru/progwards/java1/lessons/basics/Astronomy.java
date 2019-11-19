2a40488dbf89f8e581ea5b959996ae52f04e633d
Боря <Batishta_83@mail.ru>
2019-11-19 15:51:41 +0300

    Astronomy

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -16,7 +16,7 @@ public class Astronomy {
             return s;
         }
         public static Double mercurySquare(){
-            double b =2440;
+            double b =2439.7;
             double Pi = 3.14;
             double s=4*Pi*b*b;
             return s;
@@ -37,7 +37,7 @@ public class Astronomy {
             System.out.println(""+ sphereSquare((double)2));
             System.out.println(""+ earthSquare());
             System.out.println(""+mercurySquare());
-            System.out.println(jupiterSquare());
+            System.out.println(""+jupiterSquare());
             System.out.println(earthSquare()/mercurySquare());
             System.out.println(earthSquare()/jupiterSquare());
 
