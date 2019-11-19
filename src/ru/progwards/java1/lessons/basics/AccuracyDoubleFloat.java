b019b9e07005f3e828132eb916ba608272f02669
Боря <Batishta_83@mail.ru>
2019-11-19 10:55:23 +0300

    AccurasyDoubleFloat

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -1,26 +1,26 @@
 package ru.progwards.java1.lessons.basics;
-
-public class AccuracyDoubleFloat {
-    public static double volumeBallDouble(double radius){
-
+public class AccuracyDoubleFloat{
+public static double volumeBallDouble(double radius){
         return 4.0/3.0*3.14*6371.2*6371.2*6371.2;
-    }
-    public static float volumeBallFloat(float radius){
+        }
+public static float volumeBallFloat(float radius){
+        return (float) (4/3*3.14f*6371.2f*6371.2f*6371.2f);
+        }
+public static double calculateAccuracy(double radius){
+        return (4.0/3.0*3.14*6371.2*6371.2*6371.2)/(4/3*3.14f*6371.2f*6371.2f*6371.2f);
+        }
+public static void main(String[] args) {
         float Pi = 3.14f;
-        return (float) (4/3*3.14f*6371.2*6371.2*6371.2);
-    }
-    public static double calculateAccuracy(double radius){
-        return (4.0/3.0*3.14*6371.2*6371.2*6371.2)/(4/3*3.14f*6371.2*6371.2*6371.2);
-    }
-    public static void main(String[] args) {
-        float Pi = 3.14f;
+        double radius =6371.2;
+
         double volumeBallDouble= 4.0/3.0*3.14*6371.2*6371.2*6371.2;
-        double volumeBallFloat = 4/3*3.14f*6371.2*6371.2*6371.2;
-        System.out.println(volumeBallDouble);
-        System.out.println(volumeBallFloat);
+        double volumeBallFloat = 4/3*3.14f*6371.2f*6371.2f*6371.2f;
+        System.out.println(volumeBallDouble(6371.2));
+        System.out.println(volumeBallFloat(6371.2f));
         System.out.println(volumeBallDouble/volumeBallFloat);
 
-    }
-}
+        }
 
+
+        }
 
