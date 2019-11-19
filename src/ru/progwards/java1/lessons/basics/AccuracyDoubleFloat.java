270ccf40ae9c3c7d4ac6fa00dac0f6519b794f9d
Боря <Batishta_83@mail.ru>
2019-11-19 08:14:51 +0300

    AccuaracyDoubleFloat

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -3,23 +3,23 @@ package ru.progwards.java1.lessons.basics;
 public class AccuracyDoubleFloat {
     public static double volumeBallDouble(double radius){
 
-        return (radius);
+        return volumeBallDouble (radius);
     }
     public static float volumeBallFloat(float radius){
 
-        return (radius);
+        return volumeBallFloat(radius);
     }
     public static double calculateAccuracy(double radius){
-        return (radius);}
+        return calculateAccuracy (radius);
+    }
     public static void main(String[] args) {
-        double R = 6371.2;
-        float R1= 6371.2f;
-        double Pi = 3.14;
-        double a= 4/3*Pi*R*R;
-        double b= 4/3*Pi*R1*R1;
-        System.out.println("otvet " + a);
-        System.out.println("otvet " + b);
-        System.out.println("raznica " + a/b);
+        float Pi = 3.14f;
+        double volumeBallDouble= 4.0/3.0*3.14*6371.2*6371.2*6371.2;
+        double volumeBallFloat = 4/3*3.14f*6371.2*6371.2*6371.2;
+        System.out.println(volumeBallDouble);
+        System.out.println(volumeBallFloat);
+        System.out.println(volumeBallDouble/volumeBallFloat);
+
 
     }
 
