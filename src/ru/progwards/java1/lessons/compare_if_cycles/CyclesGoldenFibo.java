2c6effc46e34508c593bc369ee11c5780446a68a
Боря <Batishta_83@mail.ru>
2019-11-23 13:33:30 +0300

    TriangleInfo

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -0,0 +1,37 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class CyclesGoldenFibo {
+    public static boolean containsDigit(int number, int digit){
+        if(number==digit)
+            return true;
+        else {return false;}
+    }
+    public static int fiboNumber(int n){
+        if (n <= 3)
+        return fiboNumber (n-2);
+            else if(n<=10);
+            {return fiboNumber(n-55);}
+            //else {return (fiboNumber(n-1)+fiboNumber(n-2));}
+
+    }
+    public static boolean isGoldenTriangle(int a, int b, int c){
+        if ((a == b && b != c && a != c)&&(a <= 1.61703 && b <= 1.61903));
+        return isGoldenTriangle(a,b,c);
+    }
+
+    public static void main(String[] args) {
+        long first = 1;
+        long next = 1;
+        long current;
+        System.out.println(first);
+        System.out.println(next);
+        for (int i = 1; i <= 8; i++) {
+            current = first + next;
+            first = next;
+            next = current;
+            System.out.println(current);
+        }
+    }
+
+}
+
