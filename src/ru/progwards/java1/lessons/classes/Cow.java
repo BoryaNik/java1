07801df6b50bd405dca66766da4f1e38b65fcd0b
Боря <Batishta_83@mail.ru>
2019-11-28 12:07:51 +0300

    Cow

--- src/ru/progwards/java1/lessons/classes/Cow.java
+++ src/ru/progwards/java1/lessons/classes/Cow.java
@@ -2,12 +2,20 @@ package ru.progwards.java1.lessons.classes;
 
 public class Cow extends Animal {
     static double foodCoeff = 0.05;
-    Cow(){super(AnimalKind.COW, FoodKind.HAY,0.05,foodCoeff);}
-    Cow(double weight){super(AnimalKind.COW,FoodKind.HAY,0.05,foodCoeff);}
+
+    Cow() {
+        super(AnimalKind.COW, FoodKind.HAY, 0d, foodCoeff);
+    }
+
+    Cow(double weight) {
+        super(AnimalKind.COW, FoodKind.HAY, weight, foodCoeff);
+    }
+
 
     public static void main(String[] args) {
-        Cow animal =new Cow(0.05);
+        Cow animal = new Cow(250);
         System.out.println(animal);
-
+        System.out.println(animal.getFoodCoeff());
+        System.out.println(animal.getCoeff());
     }
 }
