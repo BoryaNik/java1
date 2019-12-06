b1c59c3c8772c83cc676aa3eb33d20aefdba5893
Боря <Batishta_83@mail.ru>
2019-12-06 17:09:03 +0300

    DIntArray

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,29 +3,37 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class DIntArray {
+    private int[] n ={};
+    DIntArray(){};
     public void add(int num){
 
-        int[] n = new int []{1,2,3};
+        int[] n1 = new int [n.length+1];//{1,2,3};
         System.out.println(Arrays.toString(n));
         n= Arrays.copyOf(n,n.length+1);
         System.out.println(Arrays.toString(n));
 
     }
     public void atInsert(int pos, int num){
-        int[] n = new int []{1,2,3};
-        System.out.println(Arrays.toString(n));
-        n= Arrays.copyOf(n,n.length+1);
+        int[] n1 = new int [n.length+1];//{1,2,3};
+        System.arraycopy(n, 0 ,n1, 0,pos);
+        System.arraycopy(n1, pos + 1, n1, pos, n1.length - pos - 1);
         System.out.println(Arrays.toString(n));
 
     }
     public void atDelete(int pos) {
-        int[] n = new int []{1,2,3};
-        System.out.println(Arrays.toString(n));
-        n= Arrays.copyOf(n,n.length-1);
-        System.out.println(Arrays.toString(n));
+        int[] n1 = new int [n.length-1];//{1,2,3};
+        //n1= Arrays.copyOf(n1,n1.length-1);
+        System.arraycopy(n, 0 ,n1, 0,pos);
+        System.arraycopy(n1, pos + 1, n1, pos, n1.length - pos - 1);
+        System.out.println(Arrays.toString(n1));
     }
 
     public static void main(String[] args) {
         System.out.println();
     }
+    public int at(int pos){
+        return n[pos];
+    }
+
 }
+
