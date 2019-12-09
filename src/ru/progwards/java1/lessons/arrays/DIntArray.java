b598bce9ae85fa3cc11c1f4b4bc9f921217320ad
Боря <Batishta_83@mail.ru>
2019-12-09 08:39:06 +0300

    DIntArray

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -10,7 +10,7 @@ public class DIntArray {
         int[] n1 = new int[n.length + 1];//{1,2,3};
         //System.out.println(Arrays.toString(n));
         //n = Arrays.copyOf(n, n.length + 1);
-        System.arraycopy(n, 0, n, 0, n.length);
+        System.arraycopy(n, 0, n1, 0, n.length);
         n1[n.length] = num;
         n = n1;
 
@@ -19,7 +19,7 @@ public class DIntArray {
     public void atInsert(int pos, int num) {
         int[] n1 = new int[n.length + 1];//{1,2,3};
         System.arraycopy(n, 0, n1, 0, pos);
-        System.arraycopy(n1, pos , n1, pos+1, n1.length - pos );
+        System.arraycopy(n, pos , n1, pos+1, n.length - pos );
         n1[pos] = num;
         n = n1;
 
@@ -29,7 +29,7 @@ public class DIntArray {
         int[] n1 = new int[n.length - 1];//{1,2,3};
         //n1= Arrays.copyOf(n1,n1.length-1);
         System.arraycopy(n, 0, n1, 0, pos);
-        System.arraycopy(n1, pos + 1, n1, pos, n1.length - pos - 1);
+        System.arraycopy(n, pos + 1, n1, pos, n.length - pos - 1);
         n = n1;
     }
 
@@ -49,6 +49,7 @@ public class DIntArray {
     public static void main(String[] args) {
 
     }
+    }
 
-}
+
 
