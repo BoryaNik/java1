38f2451c2c33277a35fc30a0dd1c1283e6b12d2f
Боря <Batishta_83@mail.ru>
2019-12-11 16:40:14 +0300

    COW

---
+++ src/ru/progwards/java1/lessons/interfaces/Hamster.java
@@ -0,0 +1,18 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class Hamster extends Animal {
+    static double foodCoeff = 0.03;
+
+    Hamster() {
+        super(AnimalKind.HAMSTER, FoodKind.CORN, 0d, foodCoeff);
+    }
+
+    Hamster(double weight) {
+        super(AnimalKind.HAMSTER, FoodKind.CORN, weight, foodCoeff);
+    }
+
+    public static void main(String[] args) {
+        Hamster animal = new Hamster(5);
+        System.out.println(animal);
+    }
+}
