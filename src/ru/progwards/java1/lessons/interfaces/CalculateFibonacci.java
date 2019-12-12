8fa6f41414611671ec4b5dd5d958c943f4889f6e
Боря <Batishta_83@mail.ru>
2019-12-12 10:59:00 +0300

    CalculateFibonacci

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -10,7 +10,7 @@ public class CalculateFibonacci {
         int n; //число, для которого рассчитываем Фибоначчи
         int fibo; //  rezultat  rascheta
 
-        CacheInfo() { n = 1; }
+        CacheInfo() { n = -1; }
 
         CacheInfo(int n, int n1) {
             this.n = n;
@@ -20,12 +20,12 @@ public class CalculateFibonacci {
     public static int fiboNumber(int n) {    //  2.1 iz zadachi 3 uroka 3 funkciya
         if (lastFibo.n == n) return lastFibo.fibo; // funkciya fiboNumber
         //   2.4 staticheskaya peremennaya lastFibo
-        int first = 1;
+        int first = 0;
         int next = 1;
         int b;
         int s = 1;
-        // while (i++ < n) {
-        for(int i=0; i<n;i++){
+         while (s++ < n) {
+        //(int i=0; i<n;i++){
             b = next;
             next += first;
             first = b;
