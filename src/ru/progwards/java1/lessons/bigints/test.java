40c7c5f7ad30f910db66963282e2ee70df8fdeaf
Боря <Batishta_83@mail.ru>
2019-12-23 10:24:24 +0300

    AbsInteger

--- src/ru/progwards/java1/lessons/bigints/test.java
+++ src/ru/progwards/java1/lessons/bigints/test.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.bigints;
 
 public class test {
     public static void main(String[] args) {
-        IntInteger i1= new IntInteger(2);
+        ShortInteger i1= new ShortInteger((short)2);
         ByteInteger i2 = new ByteInteger((byte)3);
         AbsInteger res= AbsInteger.add(i1,i2);
         System.out.println(res);
