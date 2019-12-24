1fcaf9639ff0c6312028f583859093ebe7582438
Боря <Batishta_83@mail.ru>
2019-12-24 17:08:59 +0300

    LineCount

--- src/ru/progwards/java1/lessons/io1/LineCount.java
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -1,21 +1,25 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 
 public class LineCount {
-    public static int calcEmpty(String fileName) throws Exception {
-File file = new File("data");
-        FileInputStream fis = new FileInputStream(file);
-        byte[] byteArray = new byte[(int)file.length()];
-        fis.read(byteArray);
-        String data = new String(byteArray);
-        String[] stringsArray = data.split("\r\n");
-        System.out.println(" "+ stringsArray.length);
-        return -1;
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(calcEmpty("\n"));
     }
 
-    public static void main(String[] args) {
-
+    public static int calcEmpty(String fileName) throws IOException {
+        final BufferedReader br = new BufferedReader(new StringReader("hello\n\nworld\n"));
+        String line;
+        int empty = 0;
+        int error =-1;
+        while ((line = br.readLine()) != null) {
+            if (line.trim().isEmpty()) {
+                empty++;
+            }else
+                return error;
+        }
+        System.out.println(empty);
+        return empty;
     }
 }
