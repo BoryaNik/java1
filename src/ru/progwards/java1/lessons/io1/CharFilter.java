68b914499243e7c64612ff17e1b4c504de4a6c93
Боря <Batishta_83@mail.ru>
2019-12-26 16:57:24 +0300

    CharFilter

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -5,31 +5,47 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CharFilter {
-    public static void filterFile(String inFileName,String outFileName, String filter) {
+    public static void filterFile(String inFileName, String outFileName, String filter){
         try {
-            FileReader f= new FileReader(inFileName);
-            //Scanner scanner = new Scanner(f);
+
+            // создаем экземпляр класса FileReader
+            FileReader t = new FileReader(inFileName);
+            // создаем в памяти буфер для чтения 8Кб символов за раз
             FileWriter fileWriter = new FileWriter(outFileName);
-            char[] b = new char[8096];
-            int chars = f.read(b);
-            String s= filter.replace(",","' '()-");
-            while (chars !=-1){
-                System.out.println(String.valueOf(b,0,chars));
-                chars= f.read(b);
-                //filter = inFileName.substring ("(),' '")+fileWriter;
+            char[] buffer = new char[8096];
 
+            int chars = t.read(buffer);
+
+            // до тех пор пока есть символы в файле, читаем данные
+            // и выводим в консоль
+            while (chars != -1) {
+                System.out.println(String.valueOf(buffer, 0, chars));
+                chars = t.read(buffer);
             }
-            f.close();
+
+            // закрываем файл
+            t.close();
+
+            // отлавливаем исключение
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-        catch (IOException e)
-        {e.printStackTrace();
+
+    }
+    private static String deleteFilterChars (String s, String filter) {
+        StringBuilder delet = new StringBuilder();
+        int del = s.length();
+        for (int i = 0; i < del; i++) {
+            if (filter.indexOf(s.charAt(i)) < 0)
+                delet.append(s.charAt(i));
         }
-        //catch (Exception e){filterFile("C:\\Users\\Боря\\Desktop\\java12.txt");}
+        return delet.toString();
     }
 
 
-    public static void main(String[] args)  {
-
-        filterFile("C:\\Users\\Боря\\Desktop\\java12.txt","C:\\Users\\Боря\\Desktop\\java12.txt","' '()-.");
+    public static void main(String[] args) {
+        filterFile("C:\\Users\\Боря\\Desktop\\java12.txt","\\C:\\\\Users\\\\Боря\\\\Desktop\\java12.txt\\","-' '().");
     }
+
 }
