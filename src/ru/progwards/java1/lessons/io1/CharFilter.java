3c20d13d0aa1a517c6ba17671218e3790cdeafd1
Боря <Batishta_83@mail.ru>
2019-12-27 10:03:27 +0300

    CharFilter

--- src/ru/progwards/java1/lessons/io1/CharFilter.java
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -1,38 +1,9 @@
 package ru.progwards.java1.lessons.io1;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class CharFilter {
-    public static void filterFile(String inFileName, String outFileName, String filter){
-        try {
-
-            // создаем экземпляр класса FileReader
-            FileReader t = new FileReader(inFileName);
-            // создаем в памяти буфер для чтения 8Кб символов за раз
-            FileWriter fileWriter = new FileWriter(outFileName);
-            char[] buffer = new char[8096];
-
-            int chars = t.read(buffer);
-
-            // до тех пор пока есть символы в файле, читаем данные
-            // и выводим в консоль
-            while (chars != -1) {
-                System.out.println(String.valueOf(buffer, 0, chars));
-                chars = t.read(buffer);
-            }
-
-            // закрываем файл
-            t.close();
-
-            // отлавливаем исключение
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
-
-    }
     private static String deleteFilterChars (String s, String filter) {
         StringBuilder delet = new StringBuilder();
         int del = s.length();
@@ -44,8 +15,25 @@ public class CharFilter {
     }
 
 
+    public static void filterFile(String inFileName,String outFileName, String filter) throws IOException{
+
+        FileReader fileReader = new FileReader( inFileName);
+        Scanner scanner  = new Scanner(fileReader);
+        PrintStream out = new PrintStream(new FileOutputStream(outFileName));
+        while (scanner.hasNextLine()){
+            String strFromFile = scanner.nextLine();
+            System.out.println(strFromFile);
+        }
+        try {
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
-        filterFile("C:\\Users\\Боря\\Desktop\\java12.txt","\\C:\\\\Users\\\\Боря\\\\Desktop\\java12.txt\\","-' '().");
+
     }
 
 }
