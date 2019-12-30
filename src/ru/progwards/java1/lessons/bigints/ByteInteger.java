26e10cc681f8f0d37e07a84182c5e816330f435c
Боря <Batishta_83@mail.ru>
2019-12-30 16:12:49 +0300

    AbsInteger

--- src/ru/progwards/java1/lessons/bigints/ByteInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ByteInteger.java
@@ -2,13 +2,16 @@ package ru.progwards.java1.lessons.bigints;
 
 public class ByteInteger extends AbsInteger {
     private byte number;
-    ByteInteger(byte number){
+
+    ByteInteger(byte number) {
         this.number = number;
     }
-    ByteInteger(String str){
+
+    ByteInteger(String str) {
         number = Byte.parseByte(str);
     }
-    public String toString(){
+
+    public String toString() {
         return String.valueOf(number);
     }
 
--- src/ru/progwards/java1/lessons/bigints/test.java
+++ src/ru/progwards/java1/lessons/bigints/test.java
@@ -2,10 +2,10 @@ package ru.progwards.java1.lessons.bigints;
 
 public class test {
     public static void main(String[] args) {
-        AbsInteger i1= new ByteInteger((byte)2);
-        AbsInteger i2 = new ShortInteger((short)3);
-        AbsInteger res= AbsInteger.add(i1,i2);
-        System.out.println(res);
+        //AbsInteger i1= new ByteInteger((byte)2);
+        //AbsInteger i2 = new ShortInteger((short)3);
+        //AbsInteger res= AbsInteger.add(i1,i2);
+       // System.out.println(res);
 
 
     }
