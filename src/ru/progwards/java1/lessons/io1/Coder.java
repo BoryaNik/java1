973196e5454be8d7714af90e2a7db57ebc36581e
Боря <Batishta_83@mail.ru>
2019-12-30 16:36:28 +0300

    Coder

---
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -0,0 +1,64 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.*;
+
+public class Coder {
+    public static void codeFile1(String inFileName, String outFileName, char[] code, String logName) {
+        try (
+                FileInputStream fIn = new FileInputStream(inFileName);
+                BufferedInputStream bIn = new BufferedInputStream(fIn);
+                FileOutputStream fOut = new FileOutputStream(inFileName);
+                BufferedOutputStream bOut = new BufferedOutputStream(fOut);
+        ) {
+            int i;
+            while ((i = bIn.read()) != -1) {
+                bOut.write(code[i]);
+            }
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+        FileInputStream fIn = null;
+        BufferedInputStream bIn = null;
+        FileOutputStream fOut = null;
+        BufferedOutputStream bOut = null;
+        try {
+            fIn = new FileInputStream(inFileName);
+            bIn = new BufferedInputStream(fIn);
+            fOut = new FileOutputStream(outFileName);
+            bOut = new BufferedOutputStream(fOut);
+            int i;
+            while ((i = bIn.read()) != -1) {
+                bOut.write(code[i]);
+            }
+        } catch (Throwable e) {
+            FileWriter fEOut = null;
+            BufferedWriter bEOut = null;
+            try {
+                fEOut = new FileWriter(logName, true);
+                bEOut = new BufferedWriter(fEOut);
+                bEOut.write(e.getMessage());
+            } catch (Throwable e2) {
+            } finally {
+                try {
+                    if (bEOut != null) bEOut.close();
+                    if (fEOut != null) fEOut.close();
+                } catch (Throwable e3) {
+                }
+            }
+        } finally {
+            try {
+                if (bOut != null) bOut.close();
+                if (fOut != null) fOut.close();
+                if (bIn != null) bIn.close();
+                if (fIn != null) fIn.close();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
