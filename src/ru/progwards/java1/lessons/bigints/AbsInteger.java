58ce0385e67155ed965f465a317483139987bbfc
BoryaNik <Batishta_83@mail.ru>
2020-01-06 12:13:34 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -1,17 +1,38 @@
 package ru.progwards.java1.lessons.bigints;
 
-    public abstract class AbsInteger{
-        public static AbsInteger add(AbsInteger num1, AbsInteger num2){
-            long n1 = Long.parseLong(num1.toString());
-            long n2 = Long.parseLong(num2.toString());
-            long res = n1+n2;
-            if(res > -128 && res<127)
-                return  new ByteInteger((byte)res);
+    public abstract class AbsInteger {
+        protected int b;
+        static AbsInteger add(AbsInteger num1, AbsInteger num2){
+            if (num1.b >= num2.b){
+                return num1.addTwo(num2);
+            }
             else
-                return new ShortInteger((short) res);
-
+                return num2.addTwo(num1);
         }
-
-
+        protected AbsInteger addTwo(AbsInteger num1){
+            return null;
+        }
+        protected byte byteValue(){
+            return 0;
+        }
+        protected int intValue(){
+            return 0;
+        }
+        protected short shortValue(){
+            return 0;
+        }
     }
-
+       /* public static void main(String[] args) {
+            AbsInteger num1 = new ByteInteger((byte)15);
+            AbsInteger num2 = new ShortInteger((short)314);
+            System.out.println(AbsInteger.add(num1, num2));
+            System.out.println();
+            AbsInteger num3 = new ShortInteger((short) 472);
+            AbsInteger num4 = new IntInteger(12);
+            System.out.println(AbsInteger.add(num3, num4));
+            System.out.println();
+            AbsInteger num5 = new ByteInteger((byte) 25);
+            AbsInteger num6 = new IntInteger(234);
+            System.out.println(AbsInteger.add(num5, num6));
+        }
+    }*/
--- src/ru/progwards/java1/lessons/bigints/test.java
+++ src/ru/progwards/java1/lessons/bigints/test.java
@@ -2,11 +2,17 @@ package ru.progwards.java1.lessons.bigints;
 
 public class test {
     public static void main(String[] args) {
-        AbsInteger i1= new ByteInteger((byte)2);
-        AbsInteger i2 = new ShortInteger((short)3);
-        AbsInteger res= AbsInteger.add(i1,i2);
-        System.out.println(res);
-
+        AbsInteger num1 = new ByteInteger((byte)15);
+        AbsInteger num2 = new ShortInteger((short)314);
+        System.out.println(AbsInteger.add(num1, num2));
+        System.out.println();
+        AbsInteger num3 = new ShortInteger((short) 472);
+        AbsInteger num4 = new IntInteger(12);
+        System.out.println(AbsInteger.add(num3, num4));
+        System.out.println();
+        AbsInteger num5 = new ByteInteger((byte) 25);
+        AbsInteger num6 = new IntInteger(234);
+        System.out.println(AbsInteger.add(num5, num6));
 
     }
 }
