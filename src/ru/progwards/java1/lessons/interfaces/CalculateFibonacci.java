4d6677294ce17e0601c85f15e389211d3bb39513
BoryaNik <Batishta_83@mail.ru>
2020-01-06 18:43:25 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -19,8 +19,8 @@ public class CalculateFibonacci {
     }
     public static int fiboNumber(int n) {    //  2.1 iz zadachi 3 uroka 3 funkciya
 
-            lastFibo.n= -1;
-        lastFibo.fibo =0;                 // iz primera
+        //lastFibo.n= -1;
+        lastFibo.fibo =0;       // iz primera
 
          if (lastFibo.n == n) return lastFibo.fibo; // funkciya fiboNumber
         //   2.4 staticheskaya peremennaya lastFibo
