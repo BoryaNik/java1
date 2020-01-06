216646e09914974f5340ff0624449924223379fd
BoryaNik <Batishta_83@mail.ru>
2020-01-06 21:59:20 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

---
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -0,0 +1,35 @@
+package ru.progwards.java1.lessons.bigints;
+
+import java.math.BigInteger;
+
+public class ArrayInteger {
+    byte[] digits;
+    int putVal;
+    int getVal;
+
+    ArrayInteger(int n){
+        digits = new byte[n];
+        putVal = 0;
+        getVal = 0;
+    }
+
+    void fromInt(BigInteger value){
+        digits[putVal++] = value.byteValueExact();
+    }
+
+    BigInteger toInt(){
+        byte[] rev = new byte[digits.length + 1];
+        if (getVal == putVal){
+            return BigInteger.ZERO;
+        }
+        return new BigInteger(rev);
+    }
+
+    boolean add(ArrayInteger num){
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
