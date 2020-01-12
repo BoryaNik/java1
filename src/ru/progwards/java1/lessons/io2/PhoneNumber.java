f2a1cbb86b5336a3c9d2aa10937a8a3a65e86ecf
BoryaNik <Batishta_83@mail.ru>
2020-01-12 23:56:03 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -5,11 +5,9 @@ import java.text.MessageFormat;
 
 public class PhoneNumber {
 
-
         public static final DecimalFormat phoneFormatD = new DecimalFormat("0000000000");
         public static final MessageFormat phoneFormatM = new MessageFormat("({0}){1}-{2}");
 
-        // Example Method on a Main Class
         public static void main(String[] args) {
             try {
                 System.out.println(format("8091231234"));
@@ -21,36 +19,30 @@ public class PhoneNumber {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
-
         }
 
         public static String format(String phone) throws Exception {
 
             double p = 0;
-
             if (phone instanceof String)
                 p = Double.valueOf((String) phone);
 
-
-
             if (p == 0 || String.valueOf(p) == "" || String.valueOf(p).length() < 7)
-                throw new Exception("Paramenter is no valid");
+                throw new Exception("");
 
-            String fot = phoneFormatD.format(p);
+            String str = phoneFormatD.format(p);
 
-            String extra = fot.length() > 10 ? fot.substring(0, fot.length() - 10) : "";
-            fot = fot.length() > 10 ? fot.substring(fot.length() - 10, fot.length()) : fot;
+            String num = str.length() > 10 ? str.substring(0, str.length() - 10) : "";
+            str = str.length() > 10 ? str.substring(str.length() - 10, str.length()) : str;
 
             String[] arr = {
-                    (fot.charAt(0) != '0') ? fot.substring(0, 3) : (fot.charAt(1) != '0') ? fot.substring(1, 3) : fot.substring(2, 3),
-                    fot.substring(3, 6),
-                    fot.substring(6)
+                    (str.charAt(0) != '0') ? str.substring(0, 3) : (str.charAt(1) != '0') ? str.substring(1, 3) : str.substring(2, 3),
+                    str.substring(3, 6),
+                    str.substring(6)
             };
             String r = phoneFormatM.format(arr);
             r = (r.contains("(0)")) ? r.replace("(0)", "") : r;
-            r = (extra != "") ? ("+" + extra + "" + r) : r;
+            r = (num != "") ? ("+" + num + "" + r) : r;
             return (r);
         }
     }
