0dc8a4afbce0bff4e09190f80bd04d27c9c0c8fe
BoryaNik <Batishta_83@mail.ru>
2020-01-13 23:03:46 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -4,11 +4,26 @@ import java.text.DecimalFormat;
 import java.text.MessageFormat;
 
 public class PhoneNumber {
+    private String printPhone(Long phoneNum) {
+        StringBuilder sb = new StringBuilder(15);
+        StringBuilder temp = new StringBuilder(phoneNum.toString());
 
-    public static String format(String phone){
-        long phoneNum = 123456789L;
-        System.out.println(String.valueOf(phoneNum).replaceFirst("(\\d{3})(\\d{3})(\\d+)", "+7($1)-$2$3"));
-        return phone;}
+        while (temp.length() < 10)
+            temp.insert(0, "0");
+
+        char[] chars = temp.toString().toCharArray();
+
+        sb.append("+7(");
+        for (int i = 0; i < chars.length; i++) {
+            if (i == 3)
+                sb.append(")");//+7(123)456-8989
+            else if (i == 6)
+                sb.append("-");
+            sb.append(chars[i]);
+        }
+
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
 
