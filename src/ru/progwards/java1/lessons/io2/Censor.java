6a3390792c8fcd418482f6b728a586951aab9f01
BoryaNik <Batishta_83@mail.ru>
2020-01-23 21:38:46 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

---
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -0,0 +1,85 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Censor {
+
+        public static String repeatStr(String value, int count) {
+            StringBuilder s = new StringBuilder(count);
+            for (int i = count; i > 0; i--) s.append(value);
+            return s.toString();
+        }
+
+        static class CensorException extends RuntimeException {
+
+            String errName;
+            String fileName;
+
+            CensorException(String errName, String fileName) {
+                this.errName = errName;
+                this.fileName = fileName;
+            }
+
+            @Override
+            public String toString() {
+                return errName + ":" + fileName;
+            }
+        }
+
+        public static void censorFile(String inoutFileName, String[] obscene) {
+            if (inoutFileName == null || inoutFileName.compareTo("") == 0)
+                throw new CensorException("", inoutFileName);
+            if (obscene == null) throw new CensorException("", inoutFileName);
+
+            int obLen;
+            String[] stars;
+            try {
+                obLen = obscene.length;
+                stars = new String[obLen];
+                for (int i = 0; i < obLen; i++) {
+                    stars[i] = repeatStr("*", obscene[i].length());
+                }
+            } catch (Exception e) {
+                throw new CensorException(e.getMessage(), inoutFileName);
+            }
+
+            String tmpFileName = inoutFileName + ".tmp";
+            String lineSeparator = System.getProperty("line.separator");
+            boolean firstLine = true;
+
+            try (FileReader r = new FileReader(inoutFileName); Scanner s = new Scanner(r); FileWriter w = new FileWriter(tmpFileName)) {
+                while (s.hasNext()) {
+                    if (firstLine) {
+                        firstLine = false;
+                    } else {
+                        w.write(lineSeparator);
+                    }
+                    String str = s.nextLine();
+                    for (int i = obLen - 1; i >= 0; i--) {
+                        str = str.replace(obscene[i], stars[i]);
+                    }
+                    w.write(str);
+                }
+            } catch (IOException e) {
+                throw new CensorException(e.getMessage(), inoutFileName);
+            }
+            try {
+                File f = new File(inoutFileName);
+                f.delete();
+                File n = new File(tmpFileName);
+                n.renameTo(f);
+            } catch (Exception e) {
+                throw new CensorException(e.getMessage(), inoutFileName);
+            }
+
+        }
+
+    public static void main(String[] args) {
+
+    }
+
+    }
