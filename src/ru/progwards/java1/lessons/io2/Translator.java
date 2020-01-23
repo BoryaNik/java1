a631ad0175494d8facf7697c31b9d22050883ed3
BoryaNik <Batishta_83@mail.ru>
2020-01-23 20:09:28 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

---
+++ src/ru/progwards/java1/lessons/io2/Translator.java
@@ -0,0 +1,40 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Translator {
+    Hashtable<String, String> wordTable;
+
+    Translator(String[] inLang, String[] outLang) {
+
+        if (outLang.length != inLang.length) throw new ArrayIndexOutOfBoundsException("Длины массивов должны быть одинаковы");
+
+        int count = inLang.length;
+        wordTable = new Hashtable<String, String>(count);
+        for (int i = 0; i < count; i++) {
+            wordTable.put(inLang[i].toLowerCase(), outLang[i].toLowerCase());
+        }
+    }
+
+    public String translate(String sentence) throws IOException {
+        Map<String, String> map = new HashMap<String, String>();
+
+        int len= sentence.length();
+        map.put("hello", "привет");
+        map.put("world", "мир");
+
+
+        String tr ="";
+
+        for (String s : tr.split(" ")) {
+            System.out.print(map.get(s) + " ");
+        }
+        return map.toString();
+    }
+    public static void main(String[] args){
+
+    }
+}
