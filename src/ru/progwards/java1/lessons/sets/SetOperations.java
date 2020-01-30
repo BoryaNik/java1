bddd81a650e422e5d81267a8142c7fa12f9679ae
BoryaNik <Batishta_83@mail.ru>
2020-01-30 18:48:06 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

---
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -0,0 +1,57 @@
+package ru.progwards.java1.lessons.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetOperations {
+
+        public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2){
+            Set<Integer>unionSet = new TreeSet<Integer>();
+            for(Integer currentElement : set1){
+                unionSet.add(currentElement);
+            }
+            for (Integer currentElement : set2){
+                unionSet.add(currentElement);
+            }
+            return unionSet;
+
+
+        }
+        public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2){
+            Set<Integer>intersectionSet = new TreeSet<Integer>();
+            for(Integer set1SetElement : set1){
+                for(Integer set2SetElement : set2){
+                    if(set1SetElement==set2SetElement){
+                        intersectionSet.add(set1SetElement);
+                    }
+                }
+            }
+            return intersectionSet;
+        }
+        public static Set<Integer> difference(Set<Integer> set1, Set<Integer> set2){
+            Set<Integer>differenceSet = new TreeSet<Integer>();
+            for(Integer set1SetElement : set1){
+                for(Integer set2SetElement : set2){
+                    if(set1SetElement!=set2SetElement){
+                        //differenceSet.add(set1SetElement);
+                    }
+                }
+            }
+            return differenceSet;
+        }
+        public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2){
+            Set<Integer> symDifferenceSet = new TreeSet<Integer>(set1);
+            symDifferenceSet .addAll(set2);
+            Set<Integer> tmp = new HashSet<Integer>(set1);
+            tmp.retainAll(set2);
+            symDifferenceSet .removeAll(tmp);
+            return symDifferenceSet;
+        }
+
+    public static void main(String[] args) {
+
+    }
+
+    }
+
