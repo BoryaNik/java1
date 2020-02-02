bcac23dd643d034f5e45044969eb1963ccfafa68
BoryaNik <Batishta_83@mail.ru>
2020-02-02 21:12:17 +0300

    Merge remote-tracking branch 'origin/HelloWorld' into HelloWorld
    # Conflicts:
    #	src/ru/progwards/java1/lessons/bigints/ArrayInteger.java

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -5,6 +5,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+//import static com.sun.org.apache.xml.internal.serializer.Version.getProduct;
+
 public class ProductAnalytics {
     private List<Shop> shops;
     private List<Product>products;
@@ -20,8 +22,8 @@ public class ProductAnalytics {
         Set<Product>products = new HashSet<Product>();
         return products;
     }
-    //public Set<Product>existAtListInOne(){
-    //  return existOnlyInOne();}
+    public Set<Product>existAtListInOne(){
+     return existOnlyInOne();}
     public Set<Product>notExistInShops(){
         if(products==null||products.size()==0)
             return new HashSet<>();
@@ -35,13 +37,32 @@ public class ProductAnalytics {
         return products;
     }
 
-    /* public Set<Product> existOnlyInOne(){
+    public Set<Product> existOnlyInOne(){
          Set<Product>products=new HashSet<Product>();
          if(products==null||products.size()==0)
              return products;
          if(shops==null|| shops.size()==0)
              return products;
-     }*/
+         for(int i=0; i<shops.size();i++){
+             Set<Product>products1 = new HashSet<Product>();
+         Set<Product>products2 = new HashSet<Product>();
+         Set<Product>products3;
+         Iterator n = shops.iterator();
+         for (int k=0;k<shops.size();k++){
+             products3 = new HashSet<Product>(((Shop)n.next()).getProducts());
+             products3.retainAll(products);
+             if(k<i){products1.addAll(products);}
+             else if(k==i){
+                 products2=products3;
+                 products2.removeAll(products1);
+             }else {products2.removeAll(products1);
+             }
+         }
+             products.addAll(products2);
+         }
+         return products;
+
+     }
     public static void main(String[] args) {
 
     }
